4d7324f63ec098f92048fe3d62b3cdcc231cc1bb
FelipeBarrosCode <0801.felipe@gmail.com>
2026-03-08 20:57:05 -0700

    SoftSecurity
    Needed to allow web socket type connections

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,7 +10,7 @@ const cspDirectives = [
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "script-src 'self' 'unsafe-inline'",
-  "connect-src 'self' https://testnet-api.algonode.cloud https://testnet-api.4160.nodely.dev https://testnet-idx.algonode.cloud https://testnet-idx.4160.nodely.dev https://mainnet-api.algonode.cloud https://mainnet-api.4160.nodely.dev https://mainnet-idx.algonode.cloud https://mainnet-idx.4160.nodely.dev https://api.coingecko.com https://api.frankfurter.app https://open.er-api.com https://bridge.walletconnect.org https://relay.walletconnect.com wss://relay.walletconnect.com https://wallet-connect-g.perawallet.app wss://wallet-connect-g.perawallet.app",
+  "connect-src 'self' data: https://testnet-api.algonode.cloud https://testnet-api.4160.nodely.dev https://testnet-idx.algonode.cloud https://testnet-idx.4160.nodely.dev https://mainnet-api.algonode.cloud https://mainnet-api.4160.nodely.dev https://mainnet-idx.algonode.cloud https://mainnet-idx.4160.nodely.dev https://api.coingecko.com https://api.frankfurter.app https://open.er-api.com https://bridge.walletconnect.org https://*.bridge.walletconnect.org wss://*.bridge.walletconnect.org https://relay.walletconnect.com wss://relay.walletconnect.com https://wallet-connect-g.perawallet.app wss://wallet-connect-g.perawallet.app",
   "frame-src https://www.youtube.com https://youtube.com",
   "form-action 'self'",
   "upgrade-insecure-requests",
